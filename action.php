96a6b29152998c7f58bd737b420396057354230c
Nasa1423 <Nasa1423@users.noreply.github.com>
2018-09-25 23:17:40 +0300

    Base

---
+++ action.php
@@ -0,0 +1,21 @@
+﻿<?php
+if(isset($_POST['email']) && isset($_POST['pass'])) 
+{
+    $email = htmlentities($_POST['email']);
+    $pass = htmlentities($_POST['pass']);
+    $output ="
+    <html>
+    <head>
+    <title>Анкетные данные</title>
+    </head>
+    <body>
+    Ваше мыло: $email<br />
+    Пароль: Не скажу)))"
+	$output.="</body></html>";
+    echo $output;
+}
+else
+{   
+    echo "Введенные данные некорректны";
+}
+?>
