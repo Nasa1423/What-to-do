f708e8ea5458cf5223524ab771019afbe6d72fba
Nasa1423 <40059434+Nasa1423@users.noreply.github.com>
2018-09-26 15:14:30 +0300

    Base

--- action.php
+++ action.php
@@ -3,16 +3,17 @@ if(isset($_POST['email']) && isset($_POST['pass']))
 {
     $email = htmlentities($_POST['email']);
     $pass = htmlentities($_POST['pass']);
+	require_once("register.html")
     echo "
     <html>
     <head>
     <title>Анкетные данные</title>
     </head>
     <body>
-    <p>Ваше мыло:$email</p>
+    <p>Ваше мыло:<span>$email</span></p>
     <p>Пароль: Не скажу)))</p>
 	</body>
-	</html>"
+	</html>";
 }
 else
 {   
