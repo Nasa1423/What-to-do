15500864f91a6e3c2b18daff1666a961e1d36ce2
Nasa1423 <40059434+Nasa1423@users.noreply.github.com>
2018-09-28 13:09:03 +0300

    Base

--- action.php
+++ action.php
@@ -10,22 +10,7 @@ if(isset($_POST['email']) && isset($_POST['pass']))
     <body>
     <p>Ваше мыло:<?php $email = ($_GET['email']); echo $email;?></p>
     <p>Пароль: Не скажу)))</p>
-	</body>
-<<<<<<< HEAD
-	</html>
-<?php
-=======
-	</html>";
-	end()
->>>>>>> 82bca652242867dd91364c5ee705f38efe87c332
-}
 else
 {   
     echo "Введенные данные некорректны";
-<<<<<<< HEAD
-	exit();
-=======
-	end()
->>>>>>> 82bca652242867dd91364c5ee705f38efe87c332
-}
 ?>
